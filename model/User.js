713b1ead9e7d2f2a5801c598036a10a270b2a1bc
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-02-13 21:59:51 +0545

    addd

---
+++ model/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose'); // Ensure this line is not repeated
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Basic email validation
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confirm_password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'], // Allowed roles
+        default: 'user', // Default to 'user'
+    },
+    token: {
+        type: String,
+        default: ''
+    }
+
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
