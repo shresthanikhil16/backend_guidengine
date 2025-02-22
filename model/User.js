246af76d79154c53ce3f389afc46040f2fd571e2
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-02-23 03:42:58 +0545

    changes

--- model/User.js
+++ model/User.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    profilePicture: {
+        type: String,
+        default: ''
+    },
     role: {
         type: String,
         enum: ['user', 'admin'], // Allowed roles
